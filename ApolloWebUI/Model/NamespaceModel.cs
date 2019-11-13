ad6619436852b278d970f7338883bf8d1a67781d
DaveCheng <34067983+cwbcheng@users.noreply.github.com>
2019-11-13 15:56:26 +0800

    add files

---
+++ ApolloWebUI/Model/NamespaceModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApolloWebUI.Model
+{
+    public class NamespaceModel
+    {
+        public string AppId { get; set; }
+        public string ClusterName { get; set; }
+        public string NamespaceName { get; set; }
+        public string Comment { get; set; }
+        public string Format { get; set; }
+        public bool IsPublic { get; set; }
+        public List<NamespaceItem> Items { get; set; }
+        public string DataChangeCreatedBy { get; set; }
+        public string DataChangeLastModifiedBy { get; set; }
+        public DateTime DataChangeCreatedTime { get; set; }
+        public DateTime DataChangeLastModifiedTime { get; set; }
+    }
+
+    public class NamespaceItem
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string Comment { get; set; }
+        public string DataChangeCreatedBy { get; set; }
+        public string DataChangeLastModifiedBy { get; set; }
+        public DateTime DataChangeCreatedTime { get; set; }
+        public DateTime DataChangeLastModifiedTime { get; set; }
+    }
+}
