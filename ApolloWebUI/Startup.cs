ee48148f507514dc4b0b7e4c9590456d994fe319
DaveCheng <34067983+cwbcheng@users.noreply.github.com>
2019-11-13 18:50:01 +0800

    Use Blazored.Modal

--- ApolloWebUI/Startup.cs
+++ ApolloWebUI/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ApolloWebUI.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using ApolloWebUI.Data;
+using Blazored.Modal;
 
 namespace ApolloWebUI
 {
@@ -28,7 +23,7 @@ namespace ApolloWebUI
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddBlazoredModal();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
