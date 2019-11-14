63f0bc87d1f8f71883b447ee09b1b643bc456cf1
DaveCheng <34067983+cwbcheng@users.noreply.github.com>
2019-11-14 11:25:45 +0800

    Create ApolloService.

---
+++ ApolloWebUI/Data/ApolloService.cs
@@ -0,0 +1,38 @@
+﻿using ApolloWebUI.Model;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ApolloWebUI.Data
+{
+    public class ApolloService
+    {
+        private static HttpClient httpClient = new HttpClient();
+        private IConfiguration Configuration;
+
+        public ApolloService(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public async Task<NamespaceModel> GetNamespanceAsync(string name)
+        {
+            var url = Configuration.GetValue<string>("GetApplicationUrl");
+            var key = Configuration.GetValue<string>("ApolloKey");
+            httpClient.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue(key);
+            var response = await httpClient.GetAsync(url);
+            var result = await response.Content.ReadAsStringAsync();
+            JsonSerializerOptions jsonSerializerOptions = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+            jsonSerializerOptions.Converters.Add(new DatetimeJsonConverter());
+            return JsonSerializer.Deserialize<NamespaceModel>(result, jsonSerializerOptions);
+        }
+    }
+}
--- ApolloWebUI/Startup.cs
+++ ApolloWebUI/Startup.cs
@@ -24,6 +24,7 @@ namespace ApolloWebUI
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddBlazoredModal();
+            services.AddSingleton<ApolloService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
