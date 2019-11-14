340fa55ae67ba1344f67e408a0c93db1c1c3c882
DaveCheng <34067983+cwbcheng@users.noreply.github.com>
2019-11-14 16:20:36 +0800

    Update application namespace field and release

--- ApolloWebUI/Data/ApolloService.cs
+++ ApolloWebUI/Data/ApolloService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -13,26 +14,66 @@ namespace ApolloWebUI.Data
     {
         private static HttpClient httpClient = new HttpClient();
         private IConfiguration Configuration;
+        JsonSerializerOptions jsonSerializerOptions = new JsonSerializerOptions()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        };
 
         public ApolloService(IConfiguration configuration)
         {
             this.Configuration = configuration;
+            jsonSerializerOptions.Converters.Add(new DatetimeJsonConverter());
+            var key = Configuration.GetValue<string>("ApolloKey");
+            httpClient.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue(key);
         }
 
         public async Task<NamespaceModel> GetNamespanceAsync(string name)
         {
-            var url = Configuration.GetValue<string>("GetApplicationUrl");
-            var key = Configuration.GetValue<string>("ApolloKey");
-            httpClient.DefaultRequestHeaders.Authorization =
-                new System.Net.Http.Headers.AuthenticationHeaderValue(key);
+            string apolloDomain = Configuration.GetValue<string>("ApolloDomain");
+            string appId = Configuration.GetValue<string>("ApolloAppId");
+            var url = $"{apolloDomain}openapi/v1/envs/dev/apps/{appId}/clusters/default/namespaces/application";
             var response = await httpClient.GetAsync(url);
             var result = await response.Content.ReadAsStringAsync();
-            JsonSerializerOptions jsonSerializerOptions = new JsonSerializerOptions()
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-            };
-            jsonSerializerOptions.Converters.Add(new DatetimeJsonConverter());
             return JsonSerializer.Deserialize<NamespaceModel>(result, jsonSerializerOptions);
+        }
+
+        public async Task<bool> Update(string key, string value)
+        {
+            string apolloDomain = Configuration.GetValue<string>("ApolloDomain");
+            string appId = Configuration.GetValue<string>("ApolloAppId");
+            var url = $"{apolloDomain}openapi/v1/envs/dev/apps/{appId}/clusters/default/namespaces/application/items/{key}";
+            var content = JsonSerializer.Serialize(new { key, value, comment = "a", dataChangeLastModifiedBy = "apollo" }, typeof(object), jsonSerializerOptions);
+            StringContent stringContent = new StringContent(content, Encoding.UTF8, "application/json");
+            var response = await httpClient.PutAsync(url, stringContent);
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+                return false;
+            }
+        }
+
+        public async Task<bool> Release()
+        {
+            string apolloDomain = Configuration.GetValue<string>("ApolloDomain");
+            string appId = Configuration.GetValue<string>("ApolloAppId");
+            var url = $"{apolloDomain}openapi/v1/envs/dev/apps/{appId}/clusters/default/namespaces/application/releases";
+            var content = JsonSerializer.Serialize(new { releaseTitle = DateTime.Now.ToString("yyyy-MM-dd"), releaseComment = "test", releasedBy = "apollo" }, typeof(object), jsonSerializerOptions);
+            StringContent stringContent = new StringContent(content, Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync(url, stringContent);
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+                return false;
+            }
         }
     }
 }
