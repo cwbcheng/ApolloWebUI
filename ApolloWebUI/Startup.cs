036f9ce8d885488096529dbdd9c16e6dbdf1fce7
Dave Cheng <cwb_100@qq.com>
2019-11-20 00:55:54 +0800

    Use Mysql instead of sqlite.

--- ApolloWebUI/Startup.cs
+++ ApolloWebUI/Startup.cs
@@ -25,7 +25,9 @@ namespace ApolloWebUI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(options => options.UseSqlite("DataSource=app.db"));
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseMySql(
+                    Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<AppDbContext>();
             services.AddRazorPages();
