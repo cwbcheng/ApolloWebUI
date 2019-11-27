55a6b605df0fb99df07837604a38d36995808cd9
DaveCheng <34067983+cwbcheng@users.noreply.github.com>
2019-11-27 18:30:56 +0800

    实现调用链展示

---
+++ ApolloWebUI/Model/CallChainModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApolloWebUI.Model
+{
+    public class CallChainModel
+    {
+        public List<CallChainRequest> Requests { get; set; }
+
+        public List<CallChainResponse> Responses { get; set; }
+    }
+
+    public class CallChainRequest
+    {
+        public string TraceId { get; set; }
+
+        public string SpanId { get; set; }
+
+        public string Location { get; set; }
+
+        public string Method { get; set; }
+
+        public DateTime RequestTime { get; set; }
+
+        public object Request { get; set; }
+    }
+
+    public class CallChainResponse
+    {
+        public string TraceId { get; set; }
+
+        public string SpanId { get; set; }
+
+        public DateTime ResponseTime { get; set; }
+
+        public object Response { get; set; }
+    }
+}
---
+++ ApolloWebUI/Model/SpanIdComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApolloWebUI.Model
+{
+    public class SpanIdComparer : IEqualityComparer<string>
+    {
+        public bool Equals([AllowNull] string x, [AllowNull] string y)
+        {
+            return x == y;
+        }
+
+        public int GetHashCode([DisallowNull] string obj)
+        {
+            if (obj == null)
+            {
+                return 0;
+            }
+            return obj.GetHashCode();
+        }
+    }
+}
