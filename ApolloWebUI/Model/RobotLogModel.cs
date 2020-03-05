a94e2e4be880f8574211d9cc4704ca4dcd87a412
DaveCheng <34067983+cwbcheng@users.noreply.github.com>
2020-03-05 17:18:42 +0800

    insert show robot log feature.

---
+++ ApolloWebUI/Model/RobotLogModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApolloWebUI.Model
+{
+    public class RobotLogModel
+    {
+        public long Time { get; set; }
+
+        public string RepositoryName { get; set; }
+
+        public int ProcessId { get; set; }
+
+        public string Message { get; set; }
+
+        public string Level { get; set; }
+
+        public string FormId { get; set; }
+
+        public string mac { get; set; }
+    }
+}
